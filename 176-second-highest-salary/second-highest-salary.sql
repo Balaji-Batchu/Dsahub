8a008aa780aa6a33158de1526f16116f6b3cc3d8
Balaji_Batchu <100010840+Balaji-Batchu@users.noreply.github.com>
2024-03-21 01:55:29 +0530

    Time: 754 ms (67.10%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,2 +1,4 @@
-select max(salary) as SecondHighestSalary from Employee
-where salary < (select max(salary) from Employee)
+/* Write your PL/SQL query statement below */
+select max(salary) SecondHighestSalary from Employee
+where 
+salary < (select max(salary) from Employee)
