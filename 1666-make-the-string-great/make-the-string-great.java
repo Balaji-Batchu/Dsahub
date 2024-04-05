12e7c2b150dd0b05ba409c5b2a493841032c23bb
Balaji_Batchu <100010840+Balaji-Batchu@users.noreply.github.com>
2024-04-05 17:16:20 +0530

    Time: 59 ms (7.43%) | Memory: 44.2 MB (7.82%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.java
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -6,12 +6,7 @@ class Solution {
         while (start <= end) {
             System.out.println(s);
             if (Math.abs((int) s.charAt(start) - (int) s.charAt(start + 1)) == 32) {
-                System.out.println(
-                        "Yes removing " + String.valueOf(s.charAt(start)) +
-                                String.valueOf(s.charAt(start + 1)));
-
-                s = s.deleteCharAt(start + 1);
-                s = s.deleteCharAt(start);
+                s.delete(start,start+2);
                 start = 0;
                 end = s.length() - 2;
             } else {
