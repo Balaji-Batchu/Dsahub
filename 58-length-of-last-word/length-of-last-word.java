4f8057b6d7aa3b760e950aac29be4dc2a6dfc2fc
Balaji_Batchu <100010840+Balaji-Batchu@users.noreply.github.com>
2024-04-07 18:39:13 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (27.71%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,6 +1,16 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] a = s.trim().replaceAll("\\s+"," ").split(" ");
-        return a[a.length - 1].length();
+        // String[] a = s.trim().replaceAll("\\s+"," ").split(" ");
+        // return a[a.length - 1].length();
+        int i = s.length() - 1;
+        while(i >= 0 && s.charAt(i) == ' '){
+            i--;
+        }
+        int l = 0;
+        while(i >=0 && s.charAt(i) != ' '){
+            i--;
+            l++;
+        }
+        return l;
     }
 }
