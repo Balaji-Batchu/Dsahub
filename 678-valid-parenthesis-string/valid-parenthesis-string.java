dc0ecceb4bc5a70c5ef6149c23c255373e97f6c0
Balaji_Batchu <100010840+Balaji-Batchu@users.noreply.github.com>
2024-04-07 17:06:49 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (73.12%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -2,12 +2,12 @@ class Solution {
     public boolean checkValidString(String a) {
         int leftmin = 0;
         int leftmax = 0;
-        for(Character x : a.toCharArray()){
-            if(String.valueOf(x).equals("(")){
+        for(int i = 0; i < a.length(); i++){
+            if(a.charAt(i) == '('){
                 leftmin += 1;
                 leftmax += 1;
             }
-            else if(String.valueOf(x).equals(")")){
+            else if(a.charAt(i) == ')'){
                 leftmin -= 1;
                 leftmax -= 1;
             }
@@ -15,13 +15,11 @@ class Solution {
                 leftmin -= 1;
                 leftmax += 1;
             }
-            if(leftmax < 0){
+            if(leftmax<0){
                 return false;
             }
-            if(leftmin < 0){
-                leftmin = 0;
-            }
+            leftmin=Math.max(0,leftmin);
         }
-        return leftmin == 0 ? true : false;
+        return leftmin==0;
     }
 }
