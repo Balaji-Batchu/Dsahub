c9737c553fd5a1aab3f565ba7a089014be40c827
Balaji_Batchu <100010840+Balaji-Batchu@users.noreply.github.com>
2024-06-11 22:30:45 +0530

    Time: 2 ms (65.06%) | Memory: 42.3 MB (23.04%) - LeetSync

---
+++ 1217-relative-sort-array/relative-sort-array.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int x: arr1) hm.put(x, hm.getOrDefault(x, 0) + 1);
+        int[] ans = new int[arr1.length];
+        int index = 0;
+        for(int x: arr2){
+            int n = hm.get(x);
+            while(n > 0){
+                ans[index] = x;
+                n --;
+                index ++;
+            }
+            hm.put(x, 0);
+        }
+        int[] rem = new int[arr1.length - index];
+        int i2 = 0;
+        for(Map.Entry<Integer, Integer> temp : hm.entrySet()){
+            int x = temp.getKey();
+            int n = temp.getValue();
+            while(n > 0){
+                rem[i2] = x;
+                n --;
+                i2 ++;
+            }
+            hm.put(x, 0);
+        }
+        Arrays.sort(rem);
+        for(int x: rem){
+            ans[index] = x;
+            index ++;
+        }
+        return ans;
+    }
+}
