65146e4c7db934ed9649b0e894cdd1ffa1912f61
Balaji_Batchu <100010840+Balaji-Batchu@users.noreply.github.com>
2025-09-15 23:05:50 +0530

    Time: 2 ms (91.65%) | Memory: 42.5 MB (45.48%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
@@ -1,34 +1,26 @@
 class Solution {
     public int canBeTypedWords(String text, String brokenLetters) {
-        int[] letters = new int[26];
-        int whole_words = 0;
-        
-        for(char c: brokenLetters.toCharArray()){
-            letters[c - 97] = 1;
+        boolean[] broken = new boolean[26];
+        for (char c : brokenLetters.toCharArray()) {
+            broken[c - 'a'] = true;
         }
-        System.out.println(Arrays.toString(letters));
-        
-        int broken = 0;
-        for(char c: text.toCharArray()){
-            if(c != ' '){
-                if(letters[c - 97] == 1) {
-                    System.out.println("Broken at: " + c);
-                    broken = 1;
+
+        String[] words = text.split(" ");
+        int count = 0;
+
+        for (String word : words) {
+            boolean canType = true;
+            for (char c : word.toCharArray()) {
+                if (broken[c - 'a']) {
+                    canType = false;
+                    break; // Early exit if broken letter is found
                 }
-            } 
-            
-            else {
-                if(broken == 0){
-                    System.out.println("incrementing : " + broken);
-                    whole_words ++;
-                }
-                broken = 0;
+            }
+            if (canType) {
+                count++;
             }
         }
-        
-        System.out.println("Broken: " + broken);
-        if(broken == 0) whole_words ++;
-        
-        return whole_words;
+
+        return count;
     }
 }
