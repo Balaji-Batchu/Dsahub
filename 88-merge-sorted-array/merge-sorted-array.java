7ab614880e74fa75be5f9ba66df3cc851ffb5d44
Balaji_Batchu <100010840+Balaji-Batchu@users.noreply.github.com>
2026-07-12 12:55:00 +0530

    Time: 23 ms (4.16%) | Memory: 44.7 MB (8.86%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,22 +1,30 @@
 class Solution {
-    public void merge(int[] nums1, int n, int[] nums2, int m) {
-        int i = 0, j = 0, current = 0;
-        int[] dummy = Arrays.copyOf(nums1, nums1.length);
-
-        while(i < n || j < m){
-            int left = i < n ? dummy[i] : Integer.MAX_VALUE;
-            int right = j < m ? nums2[j] : Integer.MAX_VALUE;
-
-            if(left <= right){
-                nums1[current] = left;
-                i ++;
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int pointer = nums1.length - 1;
+        int p1 = m - 1, p2 = n - 1;
+        while(p1 >= 0 && p2 >= 0) {
+            System.out.println("p1: " + pointer);
+            if(nums2[p2] >= nums1[p1]) {
+                nums1[pointer] = nums2[p2];
+                p2 --;
+            } else {
+                nums1[pointer] = nums1[p1];
+                p1 --;
             }
-            else{
-                nums1[current] = right;
-                j ++;
-            }
+            pointer --;
+            System.out.println("p1: " + p1 + ", p2: " + p2);
+        }
 
-            current ++;
+        while(p1 >= 0) {
+            nums1[pointer] = nums1[p1];
+            pointer --;
+            p1 --;
+        }
+
+        while(p2 >= 0) {
+            nums1[pointer] = nums2[p2];
+            pointer --;
+            p2 --;
         }
     }
 }
