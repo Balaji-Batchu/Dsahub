5016fd49fe8b971d8d440c3692d68596ebdef681
Balaji_Batchu <100010840+Balaji-Batchu@users.noreply.github.com>
2026-07-22 13:33:33 +0530

    Time: 24 ms (77.08%) | Memory: 49 MB (33.82%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,15 +1,16 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        HashMap<Integer, Integer> countTracker = new HashMap<>();
-        countTracker.put(0,1);
-        int sum = 0, count = 0;
+        HashMap<Integer, Integer> tracker = new HashMap<>();
+        tracker.put(0, 1);
+        int total = 0;
+        int currentSum = 0;
 
         for(int x: nums) {
-            sum += x;
-            count += countTracker.getOrDefault(sum - k, 0);
-            countTracker.put(sum, countTracker.getOrDefault(sum, 0) + 1);
+            currentSum += x;
+            total += tracker.getOrDefault((currentSum - k), 0);
+            tracker.put(currentSum, tracker.getOrDefault(currentSum, 0) + 1);
         }
-
-        return count;
+        
+        return total;
     }
 }
