fc9cbe075b24f8f6518d3dd42ea763497fb911b9
Balaji_Batchu <100010840+Balaji-Batchu@users.noreply.github.com>
2026-07-27 23:01:38 +0530

    Time: 4 ms (21.73%) | Memory: 46 MB (11.40%) - LeetSync

--- 415-add-strings/add-strings.java
+++ 415-add-strings/add-strings.java
@@ -4,41 +4,16 @@ class Solution {
         int n = num1.length() - 1, m = num2.length() - 1;
         int carry = 0;
 
-        while(n >= 0 && m >= 0) {
-            int temp = Integer.parseInt(num1.charAt(n) + "") + 
-                            Integer.parseInt(num2.charAt(m) + "") + carry;
-            carry = 0;
-            System.out.println("temp: " + temp);
+        while(n >= 0 || m >= 0 || carry != 0) {
+            String n1 = n >= 0 ? num1.charAt(n) + "" : "0";
+            String n2 = m >= 0 ? num2.charAt(m) + "" : "0";
+            int temp = Integer.parseInt(n1) + 
+                            Integer.parseInt(n2) + carry;
             ans.append(temp % 10);
-            if(temp > 9) {
-                carry = temp / 10;
-            } 
+            carry = temp / 10;
             m --;
             n --;
         }
-        while(n >= 0) {
-            int sum = carry + Integer.parseInt(num1.charAt(n) + "");
-            if(sum > 9) {
-                ans.append(sum % 10);
-                carry = sum / 10;
-            } else {
-                ans.append(sum);
-                carry = 0;
-            }
-            n --;
-        }
-        while(m >= 0) {
-            int sum = carry + Integer.parseInt(num2.charAt(m) + "");
-            if(sum > 9) {
-                ans.append(sum % 10);
-                carry = sum / 10;
-            } else {
-                ans.append(sum);
-                carry = 0;
-            }
-            m --;
-        }
-        if(carry != 0) ans.append(carry);
 
         return ans.reverse().toString();
         
