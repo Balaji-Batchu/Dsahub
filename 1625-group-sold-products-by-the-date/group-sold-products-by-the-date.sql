aa27d96009811875ae39c2c2e3967a8440f3945b
Balaji_Batchu <100010840+Balaji-Batchu@users.noreply.github.com>
2026-09-09 00:09:59 +0530

    Time: 518 ms (40.46%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1625-group-sold-products-by-the-date/group-sold-products-by-the-date.sql
@@ -0,0 +1,4 @@
+select sell_date, count(distinct product) as num_sold, 
+    GROUP_CONCAT(distinct product ORDER BY product SEPARATOR ',') AS products
+        from Activities
+        group by sell_date;
